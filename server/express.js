7e3d52756e253068470f23bb1e772ccc70aa9332
Farook Khan <farook@Farooks-MacBook-Air.local>
2017-05-29 21:32:04 -0700

    Enabling CORS

--- server/express.js
+++ server/express.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var routes = require('./router');
+var cors = require('cors');
 
 module.exports = function createApp(db) {
   const app = express();
   
+  app.use(cors());
   app.use(bodyParser.json());
   app.use('/api/applicants', routes(db));
   app.use((err, req, res, next) => {
